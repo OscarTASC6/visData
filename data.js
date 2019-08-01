c30d3929e878928d79e36a9e32d1aee35d21ddef
YousifMASC6git config --global user.name YousifMASC6git config --global user.name YousifMASC6 <Yousifmoftah@gmail.com>
2019-08-01 14:44:00 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,553 @@
+const data = {
+  results: [
+    {
+      gender: "male",
+      name: {
+        title: "mr",
+        first: "waïl",
+        last: "hulsegge"
+      },
+      location: {
+        street: "1698 potterstraat",
+        city: "eemnes",
+        state: "noord-holland",
+        postcode: 45737,
+        coordinates: {
+          latitude: "45.4622",
+          longitude: "-68.9129"
+        },
+        timezone: {
+          offset: "-11:00",
+          description: "Midway Island, Samoa"
+        }
+      },
+
+      email: "waïl.hulsegge@example.com",
+      login: {
+        uuid: "f502b3a6-ff3f-4c90-8384-cb22d691be9c",
+        username: "orangefish815",
+        password: "candyman",
+        salt: "78o0VKcI",
+        md5: "d6ec688ad829ddec8d2a5984a4eb12b5",
+        sha1: "a43c316d19b8bf2936171c01ba36a02a207a66eb",
+        sha256:
+          "2539011ffd55c2b4babf93dca1e2e6332a6da0ad55331685b2e18cb27aab7314"
+      },
+      dob: {
+        date: "1982-02-18T02:19:26Z",
+        age: 37
+      },
+      registered: {
+        date: "2005-01-06T07:56:06Z",
+        age: 14
+      },
+      phone: "(845)-079-5735",
+      cell: "(901)-459-2962",
+      id: {
+        name: "BSN",
+        value: "35226824"
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/men/14.jpg",
+        medium: "https://randomuser.me/api/portraits/med/men/14.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/14.jpg"
+      },
+      nat: "NL"
+    },
+
+    {
+      gender: "male",
+      name: {
+        title: "mr",
+        first: "مهراد",
+        last: "صدر"
+      },
+      location: {
+        street: "797 شهید گلپایگانی",
+        city: "ملارد",
+        state: "اصفهان",
+        postcode: 24363,
+        coordinates: {
+          latitude: "-27.9817",
+          longitude: "32.0659"
+        },
+        timezone: {
+          offset: "-12:00",
+          description: "Eniwetok, Kwajalein"
+        }
+      },
+      email: "مهراد.صدر@example.com",
+      login: {
+        uuid: "f0279f4f-a358-444a-946a-2e6018b12db0",
+        username: "smallduck716",
+        password: "payday",
+        salt: "YTduTaiZ",
+        md5: "0d7b094ade0701eeb386292c10960e48",
+        sha1: "ec523712911d6f2165ba9cf643e049ba8c1521a9",
+        sha256:
+          "dc5eb9fb03e201dd016ad27147966c25749faaf7e84531e142de7dbe7adfb15c"
+      },
+      dob: {
+        date: "1971-08-15T20:03:47Z",
+        age: 47
+      },
+      registered: {
+        date: "2005-03-03T23:47:23Z",
+        age: 14
+      },
+      phone: "070-11195953",
+      cell: "0967-862-9250",
+      id: {
+        name: "",
+        value: null
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/men/35.jpg",
+        medium: "https://randomuser.me/api/portraits/med/men/35.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/35.jpg"
+      },
+
+      nat: "IR"
+    },
+    {
+      gender: "male",
+      name: {
+        title: "mr",
+        first: "wyatt",
+        last: "may"
+      },
+      location: {
+        street: "1412 mcgowen st",
+        city: "red bluff",
+        state: "oregon",
+        postcode: 24838,
+        coordinates: {
+          latitude: "-31.5304",
+          longitude: "81.7624"
+        },
+        timezone: {
+          offset: "+6:00",
+          description: "Almaty, Dhaka, Colombo"
+        }
+      },
+      email: "wyatt.may@example.com",
+      login: {
+        uuid: "02dad3a8-25c0-4231-b8f9-6abd93f390f5",
+        username: "goldenkoala178",
+        password: "govols",
+        salt: "1Oyew0wI",
+        md5: "dccb159576d95f98bfe9ecc02ab24dc0",
+        sha1: "28b51711fd9a7a733a9c611c96964cc2766204d1",
+        sha256:
+          "b32782a746add00f88fdb91b8ca34da4189c02d904a9ae19bb07b9c52a5cb67f"
+      },
+      dob: {
+        date: "1981-04-29T08:31:53Z",
+        age: 38
+      },
+      registered: {
+        date: "2010-05-15T06:31:26Z",
+        age: 9
+      },
+      phone: "(371)-184-2351",
+      cell: "(069)-216-1900",
+      id: {
+        name: "SSN",
+        value: "203-44-4696"
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/men/29.jpg",
+        medium: "https://randomuser.me/api/portraits/med/men/29.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/29.jpg"
+      },
+      nat: "US"
+    },
+    {
+      gender: "female",
+      name: {
+        title: "miss",
+        first: "elizabeth",
+        last: "hughes"
+      },
+      location: {
+        street: "5490 cambridge terrace",
+        city: "palmerston north",
+        state: "bay of plenty",
+        postcode: 66869,
+        coordinates: {
+          latitude: "88.8857",
+          longitude: "22.6965"
+        },
+        timezone: {
+          offset: "+3:30",
+          description: "Tehran"
+        }
+      },
+      email: "elizabeth.hughes@example.com",
+      login: {
+        uuid: "c27bb2da-615f-4586-961b-846b23d73592",
+        username: "lazypeacock707",
+        password: "escort",
+        salt: "JnE76I2e",
+        md5: "4f09c2e8e0069f807ca0b2349eba5e36",
+        sha1: "d2431db7ae894e7a90eb1228a567dda02d1b80f0",
+        sha256:
+          "b1ffa464131c0be86cfa1fca3d817731f96a4451e8c7b3d6c61c59b9c0ef07ce"
+      },
+      dob: {
+        date: "1978-06-15T13:55:22Z",
+        age: 41
+      },
+      registered: {
+        date: "2017-10-21T19:15:47Z",
+        age: 1
+      },
+      phone: "(847)-996-4105",
+      cell: "(618)-574-2362",
+      id: {
+        name: "",
+        value: null
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/women/52.jpg",
+        medium: "https://randomuser.me/api/portraits/med/women/52.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/52.jpg"
+      },
+      nat: "NZ"
+    },
+    {
+      gender: "female",
+      name: {
+        title: "mrs",
+        first: "dorota",
+        last: "john"
+      },
+      location: {
+        street: "uhlandstraße 44",
+        city: "bad urach",
+        state: "brandenburg",
+        postcode: 74662,
+        coordinates: {
+          latitude: "-80.5083",
+          longitude: "38.5581"
+        },
+        timezone: {
+          offset: "+11:00",
+          description: "Magadan, Solomon Islands, New Caledonia"
+        }
+      },
+      email: "dorota.john@example.com",
+      login: {
+        uuid: "f42a2f10-3056-477a-aadf-34198191a937",
+        username: "bigduck732",
+        password: "1492",
+        salt: "AnPUr7uj",
+        md5: "583a960fd4dd40d7afa0e8e8369781a9",
+        sha1: "f35ab97ac69b70991b1e9f417b14d6c4c10307a9",
+        sha256:
+          "c109d77a9f326e6eacc4a8a8c1ec2cfbdce26c73bf24a8d93aadb047bd85dbc9"
+      },
+      dob: {
+        date: "1953-03-31T16:46:49Z",
+        age: 66
+      },
+      registered: {
+        date: "2003-12-24T09:37:52Z",
+        age: 15
+      },
+      phone: "0555-7303784",
+      cell: "0172-1796403",
+      id: {
+        name: "",
+        value: null
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/women/76.jpg",
+        medium: "https://randomuser.me/api/portraits/med/women/76.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/76.jpg"
+      },
+      nat: "DE"
+    },
+    {
+      gender: "male",
+      name: {
+        title: "mr",
+        first: "maël",
+        last: "roy"
+      },
+      location: {
+        street: "4329 cours charlemagne",
+        city: "tourcoing",
+        state: "meurthe-et-moselle",
+        postcode: 12297,
+        coordinates: {
+          latitude: "80.0714",
+          longitude: "152.8110"
+        },
+        timezone: {
+          offset: "-11:00",
+          description: "Midway Island, Samoa"
+        }
+      },
+      email: "maël.roy@example.com",
+      login: {
+        uuid: "8d157baa-f8d7-4bfe-a720-892e8c734a44",
+        username: "happyswan127",
+        password: "manson",
+        salt: "egR8Sh9j",
+        md5: "6380327292ba562c18bebe3af461cdba",
+        sha1: "b01d457eaa1ec51093aa0eda7ca32065df99c257",
+        sha256:
+          "2adb26122bfe6a669b84cc46788a178a0ee108067ef8ef977b767bc694978488"
+      },
+      dob: {
+        date: "1988-06-29T01:16:57Z",
+        age: 31
+      },
+      registered: {
+        date: "2003-07-19T04:48:24Z",
+        age: 16
+      },
+      phone: "04-07-84-11-83",
+      cell: "06-22-05-23-38",
+      id: {
+        name: "INSEE",
+        value: "1NNaN81652606 20"
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/men/9.jpg",
+        medium: "https://randomuser.me/api/portraits/med/men/9.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/9.jpg"
+      },
+      nat: "FR"
+    },
+    {
+      gender: "female",
+      name: {
+        title: "mrs",
+        first: "paula",
+        last: "saez"
+      },
+      location: {
+        street: "6821 calle de tetuán",
+        city: "sevilla",
+        state: "navarra",
+        postcode: 76675,
+        coordinates: {
+          latitude: "21.0564",
+          longitude: "-98.2316"
+        },
+        timezone: {
+          offset: "+5:00",
+          description: "Ekaterinburg, Islamabad, Karachi, Tashkent"
+        }
+      },
+      email: "paula.saez@example.com",
+      login: {
+        uuid: "69f1f13b-63ac-44f5-a013-b21c889c605b",
+        username: "ticklishostrich359",
+        password: "chat",
+        salt: "q78a4Izy",
+        md5: "b4aad8f696197083f28fa78cef142f00",
+        sha1: "fd32dc76255bc32332f5cb5c1e9524149f4600d4",
+        sha256:
+          "6f155af2482bbbdd8f0f73e33a2fa74d31cb15d518dab1c19fbcdef3184f664f"
+      },
+      dob: {
+        date: "1957-08-03T15:05:20Z",
+        age: 61
+      },
+      registered: {
+        date: "2017-03-28T07:33:28Z",
+        age: 2
+      },
+      phone: "926-696-000",
+      cell: "623-169-271",
+      id: {
+        name: "DNI",
+        value: "24214256-S"
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/women/65.jpg",
+        medium: "https://randomuser.me/api/portraits/med/women/65.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/65.jpg"
+      },
+      nat: "ES"
+    },
+    {
+      gender: "male",
+      name: {
+        title: "mr",
+        first: "bently",
+        last: "macdonald"
+      },
+      location: {
+        street: "630 3rd st",
+        city: "killarney",
+        state: "british columbia",
+        postcode: "D2B 5A4",
+        coordinates: {
+          latitude: "59.8447",
+          longitude: "-20.4042"
+        },
+        timezone: {
+          offset: "-11:00",
+          description: "Midway Island, Samoa"
+        }
+      },
+      email: "bently.macdonald@example.com",
+      login: {
+        uuid: "32cfa704-376a-4f06-aea0-11bfb2f296d0",
+        username: "angryswan187",
+        password: "strap",
+        salt: "RrcZn1er",
+        md5: "327af00a1bf3c611134c8dc7449c15e9",
+        sha1: "4268684e82ac24785818ff1feeeab656573515df",
+        sha256:
+          "3f30b1ef225cfa02e1ae3d878a9ef7e25bd7a5bc2953c9a344f1787f4bd54746"
+      },
+      dob: {
+        date: "1946-11-22T00:43:08Z",
+        age: 72
+      },
+      registered: {
+        date: "2016-08-24T14:49:55Z",
+        age: 2
+      },
+      phone: "874-181-1239",
+      cell: "102-119-9245",
+      id: {
+        name: "",
+        value: null
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/men/82.jpg",
+        medium: "https://randomuser.me/api/portraits/med/men/82.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/82.jpg"
+      },
+      nat: "CA"
+    },
+    {
+      gender: "male",
+      name: {
+        title: "mr",
+        first: "geoffry",
+        last: "van de ridder"
+      },
+      location: {
+        street: "6982 oudkerkhof",
+        city: "lisse",
+        state: "groningen",
+        postcode: 37752,
+        coordinates: {
+          latitude: "59.3941",
+          longitude: "-150.5589"
+        },
+        timezone: {
+          offset: "+1:00",
+          description: "Brussels, Copenhagen, Madrid, Paris"
+        }
+      },
+      email: "geoffry.vanderidder@example.com",
+      login: {
+        uuid: "1b0bc722-d434-4083-95dc-2a84a0a406cc",
+        username: "redrabbit861",
+        password: "daewoo",
+        salt: "dtRGBriG",
+        md5: "aa927efad7aa686a4906aa0ec458dc15",
+        sha1: "25f2f569248c13176894aa9402b6ea39150aee3e",
+        sha256:
+          "fd13c569dacd65986c642a24d559cbed05c2f3db51b7db288f20ab51ad4f5707"
+      },
+      dob: {
+        date: "1974-12-21T02:36:44Z",
+        age: 44
+      },
+      registered: {
+        date: "2004-05-26T17:48:37Z",
+        age: 15
+      },
+      phone: "(071)-171-5951",
+      cell: "(065)-117-1458",
+      id: {
+        name: "BSN",
+        value: "41196816"
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/men/32.jpg",
+        medium: "https://randomuser.me/api/portraits/med/men/32.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/32.jpg"
+      },
+      nat: "NL"
+    },
+    {
+      gender: "male",
+      name: {
+        title: "mr",
+        first: "alberto",
+        last: "montero"
+      },
+      location: {
+        street: "702 calle del arenal",
+        city: "almería",
+        state: "castilla y león",
+        postcode: 93543,
+        coordinates: {
+          latitude: "60.0406",
+          longitude: "-10.6145"
+        },
+        timezone: {
+          offset: "-8:00",
+          description: "Pacific Time (US & Canada)"
+        }
+      },
+      email: "alberto.montero@example.com",
+      login: {
+        uuid: "aaadeb4f-f0e8-4663-9b43-ea4aae900035",
+        username: "crazygorilla306",
+        password: "shoes",
+        salt: "shTMkgyx",
+        md5: "02671e244ec7ddceda72f1881cec3ba0",
+        sha1: "36e67d885404dc2ba044507ec3547c0a24ca3c08",
+        sha256:
+          "22fbb79c55d6f5ff7fb6d4aecb1123898a999c89698ec74769bdd6349485da14"
+      },
+      dob: {
+        date: "1945-08-25T17:15:22Z",
+        age: 73
+      },
+      registered: {
+        date: "2010-05-10T03:54:19Z",
+        age: 9
+      },
+      phone: "918-762-642",
+      cell: "680-382-890",
+      id: {
+        name: "DNI",
+        value: "09659128-X"
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/men/73.jpg",
+        medium: "https://randomuser.me/api/portraits/med/men/73.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/73.jpg"
+      },
+      nat: "ES"
+    }
+  ],
+  info: {
+    seed: "34d2f1c3e93534ce",
+    results: 10,
+    page: 1,
+    version: "1.2"
+  }
+};
+function certain() {
+  var text = "";
+  for (let i = 0; i < 10; i++) {
+    text += data[i] + "<br>";
+    console.log(data.results[i].dob);
+  }
+}
+// var downRow = document.querySelectorAll(".downrow");
+// // downRow.innerHTML = "HELLO!";     
+// document.getElementById("downrow").innerHTML = "New text!"; 
